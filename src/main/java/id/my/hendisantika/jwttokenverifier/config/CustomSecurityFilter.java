ff16a6e545be356a66b7cfef62b6174c4607753a
hendisantika <hendisantika@yahoo.co.id>
2025-04-17 09:25:54 +0700

    feat: Add doFilter in CustomSecurityFilter🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/jwttokenverifier/config/CustomSecurityFilter.java
+++ src/main/java/id/my/hendisantika/jwttokenverifier/config/CustomSecurityFilter.java
@@ -1,12 +1,29 @@
 package id.my.hendisantika.jwttokenverifier.config;
 
+import com.auth0.jwk.Jwk;
 import com.auth0.jwk.JwkProvider;
 import com.auth0.jwk.JwkProviderBuilder;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.logging.Filter;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,11 +38,42 @@ import java.util.logging.Filter;
  */
 @Slf4j
 public class CustomSecurityFilter implements Filter {
-
-
     private final JwkProvider jwkProvider;
 
     public CustomSecurityFilter() throws MalformedURLException {
         jwkProvider = new JwkProviderBuilder(new URL("http://localhost:8080/realms/master/protocol/openid-connect/certs")).build();
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+
+            String authorizationHeader = ((HttpServletRequest) request).getHeader("Authorization");
+            String token = authorizationHeader.substring(7);
+
+            DecodedJWT decodedJWT = JWT.decode(token);
+
+            // start verification process
+            Jwk jwk = jwkProvider.get(decodedJWT.getKeyId());
+
+            // Assuming all tokens are signed using RSA256 algorithm otherwise algorithm can be found using jwk.getAlgorithm() method
+            Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
+
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer("http://localhost:8080/realms/master")
+                    .withAudience("backend-api")
+                    .build();
+            verifier.verify(decodedJWT);
+
+            SecurityContextHolder.getContext().setAuthentication(
+                    new UsernamePasswordAuthenticationToken(decodedJWT.getSubject(), "***",
+                            List.of(new SimpleGrantedAuthority("SIMPLE_AUTHORITY"))));
+        } catch (JWTVerificationException jwtVerificationException) {
+            log.error("Verification Exception", jwtVerificationException);
+        } catch (Exception e) {
+            log.error("Exception", e);
+        }
+        chain.doFilter(request, response);
+        SecurityContextHolder.clearContext();
     }
 }
