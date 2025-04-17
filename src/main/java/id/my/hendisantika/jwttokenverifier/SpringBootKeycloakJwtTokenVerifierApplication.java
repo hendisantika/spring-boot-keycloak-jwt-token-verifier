27f62650ea610639aadbff578ffd729599b1101b
hendisantika <hendisantika@yahoo.co.id>
2025-04-17 09:14:43 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwttokenverifier/SpringBootKeycloakJwtTokenVerifierApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jwttokenverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKeycloakJwtTokenVerifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootKeycloakJwtTokenVerifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/jwttokenverifier/SpringBootKeycloakJwtTokenVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.jwttokenverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKeycloakJwtTokenVerifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
