d76cb708bb4576d69a5d8db73e4ac810c17353b9
hendisantika <hendisantika@yahoo.co.id>
2025-04-17 09:27:10 +0700

    feat: Add SecureController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/jwttokenverifier/contoller/SecureController.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.jwttokenverifier.contoller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : jwt-token-verifier
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 17/04/25
+ * Time: 09.26
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/secure-api")
+public class SecureController {
+}
