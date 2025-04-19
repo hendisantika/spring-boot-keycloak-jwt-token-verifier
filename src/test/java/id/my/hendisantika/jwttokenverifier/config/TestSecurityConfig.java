b0913952fd7215667f02d1673244062cdb2d96e1
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 12:15:42 +0700

    feat: Add TestSecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/jwttokenverifier/config/TestSecurityConfig.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.jwttokenverifier.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@TestConfiguration
+@EnableMethodSecurity
+public class TestSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvcMatcherBuilder = new MvcRequestMatcher.Builder(introspector);
+
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(mvcMatcherBuilder.pattern("/**")).authenticated())
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(withDefaults())
+                .build();
+    }
+}
