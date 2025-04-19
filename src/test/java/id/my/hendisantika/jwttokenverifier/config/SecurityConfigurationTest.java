a00831bf05a5da96620eb37fb48bbef200a9781d
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 12:21:40 +0700

    fix: Integration Test🫸🌀✏️📗🐧🐳

--- src/test/java/id/my/hendisantika/jwttokenverifier/config/SecurityConfigurationTest.java
+++ src/test/java/id/my/hendisantika/jwttokenverifier/config/SecurityConfigurationTest.java
@@ -1,17 +1,19 @@
 package id.my.hendisantika.jwttokenverifier.config;
 
+import id.my.hendisantika.jwttokenverifier.contoller.EmployeeController;
+import id.my.hendisantika.jwttokenverifier.contoller.SecureController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest({EmployeeController.class, SecureController.class})
+@Import(TestSecurityConfig.class)
 public class SecurityConfigurationTest {
 
     @Autowired
