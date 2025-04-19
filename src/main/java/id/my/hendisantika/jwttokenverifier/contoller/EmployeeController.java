78690515c69874457892642e21633b3e5f8523ab
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 11:59:36 +0700

    feat: Add readEmployee in EmployeeController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/jwttokenverifier/contoller/EmployeeController.java
+++ src/main/java/id/my/hendisantika/jwttokenverifier/contoller/EmployeeController.java
@@ -1,7 +1,10 @@
 package id.my.hendisantika.jwttokenverifier.contoller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,4 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+    @PreAuthorize("hasAuthority('postman-client.employee') or hasAuthority('postman-client.supervisor')")
+    @RequestMapping("/read")
+    public Map<String, String> readEmployee() {
+        return Map.of("name", "Yuji", "role", "Developer", "age", "25");
+    }
 }
